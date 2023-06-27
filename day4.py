4a4fbb6185bae1d746c25b7b844380453917a3ed
kanikasawant <137372086+kanikasawant@users.noreply.github.com>
2023-06-27 22:48:38 +0530

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,53 @@
+#for with else loop
+for i in "kanika":
+    if i=="c":
+        break
+    else:
+        print(i)
+else:
+    print("This has break")
+
+# for i in "kanika":
+#      if i=="n":
+#          pass
+#      else:
+#          print(i)
+# else:
+#      print("This has pass")
+
+
+# for i in "kanika":
+#      if i=="r":
+#          continue
+#      else:
+#          print(i)
+# else:
+#      print("This has continue")
+
+# ##while with else loop
+# i=1
+# while i<=10:
+#      if i==5:
+#          break
+#      else:
+#          print(i)
+# else:
+#      print("This has break")
+
+# i=1
+# while i<=10:
+#      if i==5:
+#          continue
+#      else:
+#          print(i)
+# else:
+#      print("This has continue")
+
+# i=1
+# while i<=10:
+#      if i==5:
+#          pass
+#      else:
+#          print(i)
+# else:
+#      print("This has pass")
